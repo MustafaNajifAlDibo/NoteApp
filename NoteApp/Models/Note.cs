8b2141a581ec798f16ab10949efd3b92795b2069
Mustafa Najif Al-Dibo <mustafa.najif@outlook.com>
2026-06-26 11:39:51 +0200

    create Not Model

---
+++ NoteApp/Models/Note.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoteApp.Models {
+    public class Note {
+
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
