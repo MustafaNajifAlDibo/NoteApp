7aa47894ea758d0ad4ae7e69584bea0684af88af
Mustafa Najif Al-Dibo <mustafa.najif@outlook.com>
2026-06-26 12:21:41 +0200

    create Add- & RemoveNote Commands to NoteViewModel

--- NoteApp/ViewModels/NoteViewModel.cs
+++ NoteApp/ViewModels/NoteViewModel.cs
@@ -2,14 +2,15 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace NoteApp.ViewModels {
     public partial class NoteViewModel : INotifyPropertyChanged{
 
         // Fields
-        private string? _noteTitle;
-        private string? _noteDescription;
-        private Note? _selectedNote;
+        private string _noteTitle;
+        private string _noteDescription;
+        private Note _selectedNote;
 
         // get set
         public string NoteTitle {
@@ -43,13 +44,45 @@ namespace NoteApp.ViewModels {
         }
 
         // Properties
-        public ObservableCollection<Note>? NoteCollection { get; set; }
+        public ObservableCollection<Note> NoteCollection { get; set; }
 
-
+        // Commands
+        public ICommand AddNoteCommand { get;}
+        public ICommand EditNoteCommand { get; }
+        public ICommand RemoveNoteCommand { get; }
 
         // Constructor
         public NoteViewModel() {
             NoteCollection = new ObservableCollection<Note>();
+
+            AddNoteCommand = new Command(AddNote);
+            AddNoteCommand = new Command(DeleteNote);
+        }
+
+        private void DeleteNote(object obj) {
+            if(SelectedNote != null) {
+                NoteCollection.Remove(SelectedNote);
+                // Reset Values
+                NoteTitle = string.Empty;
+                NoteDescription = string.Empty;
+            }
+        }
+
+        private void AddNote(object obj) {
+
+            // Generatte a Unique ID for the new Note
+            int newId = NoteCollection.Count > 0 ? 
+                NoteCollection.Max(x => x.Id) + 1: 1;
+            // Set New Note
+            var note = new Note {
+                Title = NoteTitle,
+                Description = NoteDescription
+            };
+            NoteCollection.Add(note);
+
+            // Reset Values
+            NoteTitle = string.Empty;
+            NoteDescription = string.Empty;
         }
 
 
